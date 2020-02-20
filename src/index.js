ed7ce4e0c7dcffa60f72b2d662894a38953c539c
Eugene Gunev <eugene.gunev@gmail.com>
2020-02-20 11:58:34 +0300

    fix formatting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const { fetchJoke } = require('./helpers/jokeAPI');
+const {fetchJoke} = require('./helpers/jokeAPI');
 const {
   botToken,
   botPort,
@@ -8,16 +8,16 @@ const {
   dark,
   miscellaneous,
   any,
-  introduction
+  introduction,
 } = require('./constants');
 
-// const bot = new TelegramBot(botToken, { polling: true });
 const botOptions = {
   webHook: {
     port: botPort,
-  }
-}
+  },
+};
 
+// const bot = new TelegramBot(botToken, { polling: true });
 const bot = new TelegramBot(botToken, botOptions);
 bot.setWebHook(`${botHerokuURL}/bot${botToken}`);
 
@@ -28,50 +28,42 @@ const displayJoke = (category, msg) => {
 
   fetchJoke(category)
     .then(response => response.json())
-    .then(data => jokeInfo = data)
+    .then(data => (jokeInfo = data))
     .then(() => {
-      jokeInfo && jokeInfo.type === 'single' ?
-        bot.sendMessage(
-          msg.chat.id,
-          `<em>Here's your ${category} joke:</em> \n\n${jokeInfo.joke}`,
-          { parse_mode: "HTML" }
-        ) :
-        bot.sendMessage(
-          msg.chat.id,
-          `<em>Here's your joke:</em> \n\n${jokeInfo.setup} \n<b>${jokeInfo.delivery}</b>`,
-          { parse_mode : "HTML" }
-        )
+      jokeInfo && jokeInfo.type === 'single'
+        ? bot.sendMessage(
+            msg.chat.id,
+            `<em>Here's your ${category} joke:</em> \n\n${jokeInfo.joke}`,
+            {parse_mode: 'HTML'},
+          )
+        : bot.sendMessage(
+            msg.chat.id,
+            `<em>Here's your joke:</em> \n\n${jokeInfo.setup} \n<b>${jokeInfo.delivery}</b>`,
+            {parse_mode: 'HTML'},
+          );
     })
-    .catch(err => console.log(err));
-}
+    .catch(err => console.error(err));
+};
 
 bot.onText(/\/help/, msg => {
-  bot.sendMessage(
-    msg.chat.id,
-    introduction,
-    { parse_mode: "HTML" }
-  )
-})
+  bot.sendMessage(msg.chat.id, introduction, {parse_mode: 'HTML'});
+});
 
 bot.onText(/\/start|\/select/, msg => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Which topic would you like to laugh at today?",
-    {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: programming.keyboardOption, callback_data: programming.description },
-            { text: dark.keyboardOption, callback_data: dark.description }
-          ],
-          [
-            { text: miscellaneous.keyboardOption, callback_data: miscellaneous.description },
-            { text: any.keyboardOption, callback_data: any.description }
-          ]
-        ]
-      }
-    }
-  )
+  bot.sendMessage(msg.chat.id, 'Which topic would you like to laugh at today?', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {text: programming.keyboardOption, callback_data: programming.description},
+          {text: dark.keyboardOption, callback_data: dark.description},
+        ],
+        [
+          {text: miscellaneous.keyboardOption, callback_data: miscellaneous.description},
+          {text: any.keyboardOption, callback_data: any.description},
+        ],
+      ],
+    },
+  });
 });
 
 bot.on('callback_query', callbackQuery => {
@@ -79,7 +71,7 @@ bot.on('callback_query', callbackQuery => {
   const queryData = callbackQuery.data;
   const msg = callbackQuery.message;
 
-  bot.answerCallbackQuery(queryId, { text: "Enjoy the joke!" });
+  bot.answerCallbackQuery(queryId, {text: 'Enjoy the joke!'});
 
   if (queryData.includes(programming.description)) {
     displayJoke(programming.description, msg);
@@ -93,47 +85,44 @@ bot.on('callback_query', callbackQuery => {
 });
 
 bot.on('inline_query', msg => {
-  bot.answerInlineQuery(
-    msg.id,
-    [
-      {
-        type: "article",
-        id: programming.description,
-        title: programming.description,
-        thumb_url: programming.imgURL,
-        input_message_content: {
-          message_text: programming.description
-        }
+  bot.answerInlineQuery(msg.id, [
+    {
+      type: 'article',
+      id: programming.description,
+      title: programming.description,
+      thumb_url: programming.imgURL,
+      input_message_content: {
+        message_text: programming.description,
       },
-      {
-        type: "article",
-        id: dark.description,
-        title: dark.description,
-        thumb_url: dark.imgURL,
-        input_message_content: {
-          message_text: dark.description
-        }
+    },
+    {
+      type: 'article',
+      id: dark.description,
+      title: dark.description,
+      thumb_url: dark.imgURL,
+      input_message_content: {
+        message_text: dark.description,
       },
-      {
-        type: "article",
-        id: miscellaneous.description,
-        title: miscellaneous.description,
-        thumb_url: miscellaneous.imgURL,
-        input_message_content: {
-          message_text: miscellaneous.description
-        }
+    },
+    {
+      type: 'article',
+      id: miscellaneous.description,
+      title: miscellaneous.description,
+      thumb_url: miscellaneous.imgURL,
+      input_message_content: {
+        message_text: miscellaneous.description,
       },
-      {
-        type: "article",
-        id: any.description,
-        title: any.description,
-        thumb_url: any.imgURL,
-        input_message_content: {
-          message_text: any.description
-        }
-      }
-    ]
-  )
+    },
+    {
+      type: 'article',
+      id: any.description,
+      title: any.description,
+      thumb_url: any.imgURL,
+      input_message_content: {
+        message_text: any.description,
+      },
+    },
+  ]);
 });
 
 bot.on('message', msg => {
